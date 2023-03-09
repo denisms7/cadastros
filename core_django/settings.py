398c3ea34c389ddfa4658dbc54bce693c975e10c
Denis Muniz Silva <82631808+denisms7@users.noreply.github.com>
2023-03-09 11:43:23 -0300

    correcao js

--- core_django/settings.py
+++ core_django/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
+    'django.contrib.humanize',
 
     'cadastros.apps.CadastrosConfig',
     'nav_bar.apps.NavBarConfig',
--- core_django/urls.py
+++ core_django/urls.py
@@ -6,8 +6,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('cadastros.urls')),
+    path('cadastro/', include('cadastros.urls')),
+
+    path('', include('nav_bar.urls')),
 ]
 
-if settings.DEBUG == True:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG == True:
+#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
