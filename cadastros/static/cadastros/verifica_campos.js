b490878dfe31e5f04a4082e1d7faa1185844d3ca
Denis Muniz Silva <82631808+denisms7@users.noreply.github.com>
2023-05-26 14:08:22 -0300

    added expandir bloco preenchido

---
+++ cadastros/static/cadastros/verifica_campos.js
@@ -0,0 +1,43 @@
+function Verifica(var_classe) {
+    var verificaCamposDiv = document.querySelector(var_classe);
+    var inputs = verificaCamposDiv.getElementsByTagName('input');
+    var selects = verificaCamposDiv.getElementsByTagName('select');
+    var todosVazios = true;
+
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].value !== '' && inputs[i].value !== '0') {
+            todosVazios = false;
+            break;
+        }
+    }
+
+    if (todosVazios) {
+        for (var j = 0; j < selects.length; j++) {
+            if (selects[j].value !== '' && selects[j].value !== '0') {
+                todosVazios = false;
+                break;
+            }
+        }
+    }
+    return todosVazios
+}
+
+
+function verificarCampos(classe, btncheckId, elementId) {
+    if (Verifica(classe)) {
+        // Todos os campos estão vazios dentro da div
+        document.getElementById(btncheckId).checked = false;
+        document.getElementById(elementId).classList.remove('show');
+    } else {
+        // Existem campos preenchidos dentro da div
+        document.getElementById(btncheckId).checked = true;
+        document.getElementById(elementId).classList.add('show');
+    }
+}
+
+verificarCampos('.Verifica_Contato', 'btncheck1', 'idContato');
+verificarCampos('.Verifica_Endereco', 'btncheck2', 'idEndereco');
+verificarCampos('.Verifica_Banco', 'btncheck3', 'idBanco');
+
+
+
