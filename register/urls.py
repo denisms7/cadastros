af1cea1ee652af1a127b55f73002e73ba29d46d3
Denis Muniz Silva <denis.m.s.777@hotmail.com>
2026-01-04 01:59:09 -0300

    added

--- register/urls.py
+++ register/urls.py
@@ -10,17 +10,17 @@ app_name = 'register'
 urlpatterns = [
     # Pessoa
     path('pf/', Pf_ListView.as_view(), name='person_'),
-    path('pf/add/', Pf_CreateView.as_view(), name='person_add'),
-    path('pf/<int:pk>/', Pf_DetailView.as_view(), name='person_detail'),
-    path('pf/<int:pk>/edit/', Pf_UpdateView.as_view(), name='person_change'),
-    path('pf/<int:pk>/del/', Pf_DeleteView.as_view(), name='person_delete'),
+    path('pf/0/adicionar/', Pf_CreateView.as_view(), name='person_add'),
+    path('pf/<int:pk>/visualizar/', Pf_DetailView.as_view(), name='person_detail'),
+    path('pf/<int:pk>/editar/', Pf_UpdateView.as_view(), name='person_change'),
+    path('pf/<int:pk>/deletar/', Pf_DeleteView.as_view(), name='person_delete'),
     # Empresa
     path('pj/', Pj_ListView.as_view(), name='enterprise_'),
-    path('pj/add/', Pj_CreateView.as_view(), name='enterprise_add'),
-    path('pj/<int:pk>/', Pj_DetailView.as_view(), name='enterprise_detail'),
-    path('pj/<int:pk>/edit/', Pj_UpdateView.as_view(), name='enterprise_edit'),
-    path('pj/<int:pk>/del/', Pj_DeleteView.as_view(), name='enterprise_delete'),
+    path('pj/0/adicionar/', Pj_CreateView.as_view(), name='enterprise_add'),
+    path('pj/<int:pk>/visualizar/', Pj_DetailView.as_view(), name='enterprise_detail'),
+    path('pj/<int:pk>/editar/', Pj_UpdateView.as_view(), name='enterprise_edit'),
+    path('pj/<int:pk>/deletar/', Pj_DeleteView.as_view(), name='enterprise_delete'),
     # Utilitarios
     path('agenda/', Contacts_ListView.as_view(), name='contacts_'),
-    path('cad/<int:pk>/log/', Log_View.as_view(), name='register_log'),
+    path('cadastro/<int:pk>/log/', Log_View.as_view(), name='register_log'),
 ]
